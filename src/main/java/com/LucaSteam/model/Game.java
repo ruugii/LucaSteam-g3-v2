a19e6ae3b34c4d1edcfaee416a147224a1c1a67a
ruugii_31 <90140860+ruugii@users.noreply.github.com>
2023-08-23 10:47:05 +0200

    finish Entity Game

--- src/main/java/com/LucaSteam/model/Game.java
+++ src/main/java/com/LucaSteam/model/Game.java
@@ -1,7 +1,6 @@
 package com.LucaSteam.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "game")
@@ -9,6 +8,42 @@ public class Game {
     private long id;
     private String name;
     private String description;
-    private String image;
 
+    public Game() {
+        super();
+    }
+
+    public Game(String name, String description) {
+        super();
+        this.name = name;
+        this.description = description;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_game")
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }

afe7d4c87e360efb36158ee91eb2857209283d70
JeSuns <jesuslopezborrego@gmail.com>
2023-08-23 13:15:19 +0200

    Jesus: upload Service Interface

---
+++ src/main/java/com/LucaSteam/service/GameService.java
@@ -0,0 +1,20 @@
+package com.LucaSteam.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.LucaSteam.model.Game;
+
+public interface GameService {
+	
+	public List<Game> findAll();
+	
+	public void save(Game game);
+	
+	public Optional<Game> update(Game game);
+	
+	public Optional<Game> findById(int id);
+	
+	public void deleteById(int id);
+	
+}
